b8d2cf970460be91b1ade6d932477919d9f34fbf
isamu-isozaki <isamu.website@gmail.com>
2020-11-12 18:07:10 -0500

    Implemented early stopping

--- binary_search_networks/train.py
+++ binary_search_networks/train.py
@@ -3,13 +3,18 @@
 # Purpose: Take X, y, and n as input as well as parser arguments. Prints accuracy and return model
 
 from binary_search_networks.model import model
+from keras.callbacks import EarlyStopping
 
 def train(X, y, **args):
     train_model = model(**args)
     train_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     # running the fitting
-    train_model.fit(X, y, epochs=args['epoch'], batch_size=args['batch_size'], validation_split=args['validation_split'], verbose = args['verbose'])
+    callback = EarlyStopping(monitor='loss', patience=3)
+    if args['no_early_stopping']:
+        train_model.fit(X, y, epochs=args['epoch'], batch_size=args['batch_size'], validation_split=args['validation_split'], verbose = args['verbose'])
+    else:
+        train_model.fit(X, y, epochs=args['epoch'], batch_size=args['batch_size'], validation_split=args['validation_split'], verbose = args['verbose'], callbacks=[callback])
     _, accuracy = train_model.evaluate(X, y)
     print('Train Accuracy: %.2f' % (accuracy*100))
     return train_model
