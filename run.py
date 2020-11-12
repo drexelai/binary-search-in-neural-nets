c5de10b608b323d1b0211b88dfe0ec8d14ae3469
isamu-isozaki <isamu.website@gmail.com>
2020-11-12 17:46:20 -0500

    Made run_model with argument n with default parameters

--- run.py
+++ run.py
@@ -2,15 +2,12 @@
 # Date: 2020/11/10
 # Purpose: Take n as input and run program
 
-from binary_search_networks.binary_search_parser import binary_search_parser 
 from binary_search_networks.pipeline import run_pipe
+from binary_search_networks.util import parse_arguments
 import sys
 def main(args):
-	arg_parser = binary_search_parser()
-	args, _ = arg_parser.parse_known_args(args)#the openai way
-	#convert args to dictionary
-	args = vars(args)
-
+	args = parse_arguments(args)
 	run_pipe(**args)
+	
 if __name__ == "__main__":
 	main(sys.argv)
