8f3d0ca84c8fce3738589240abbfb5748632a1ea
ethan <57536747+ethanmoyer@users.noreply.github.com>
2020-11-13 11:12:43 -0500

    Initial implementation of binary search

--- run.py
+++ run.py
@@ -4,10 +4,24 @@
 
 from binary_search_networks.pipeline import run_pipe
 from binary_search_networks.util import parse_arguments
+
+from binary_search_networks.search import binary_search, get_output_space, plot_output_space
+
 import sys
 def main(args):
 	args = parse_arguments(args)
-	run_pipe(**args)
+
+	print(args)
+
+	#a, b, accuracys = get_output_space(**args)
+	#plot_output_space(a, b, accuracys)
+
+	binary_search(**args)
+
+	#run_pipe(**args)
+
+
+
 	
 if __name__ == "__main__":
 	main(sys.argv)
