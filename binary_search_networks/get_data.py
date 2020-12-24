e0aba23f0fe8a4a012b5c9fe27359ec8c8e34835
isamu-isozaki <isamu.website@gmail.com>
2020-12-24 10:48:15 -0500

    update

--- binary_search_networks/get_data.py
+++ binary_search_networks/get_data.py
@@ -8,7 +8,7 @@ from sklearn.impute import SimpleImputer
 import pandas as pd
 import numpy as np
 
-def get_data(**args):
+def get_titanic_data(**args):
     data = pd.read_csv('titanic.csv')
     X, y = data[['pclass', 'sex', 'age', 'sibsp', 'parch', 'fare', 'embarked']], data['survived']
     X[['age', 'fare']]=X[['age', 'fare']].replace('?', np.nan)
@@ -17,4 +17,42 @@ def get_data(**args):
     X = pd.get_dummies(X, columns=['sex', 'embarked'])
     print(args)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=args['test_size'], random_state=42)
+    return X_train, X_test, y_train, y_test
+
+
+def get_data_churn_rate(**args):
+    # data = pd.read_csv('ChurnModel.csv')
+    # X, y = data[["TQMScore","Geography","ProductType",	"TotalCustomerYears","ContractDurationInMonths","RevenueInMillions","NumOfProducts"	,"RenewedBefore","IsActiveMember","MaxAttentionContractCost"
+    # ]], data['Exited']
+    # X = pd.get_dummies(X, columns=['Geography', 'ProductType'])
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=args['test_size'], random_state=42)
+    dataset = pd.read_csv('ChurnModel.csv')
+    X = dataset.iloc[:, 3:13].values
+    #We store the Dependent value/predicted value in y by storing the 13th index in the variable y
+    y = dataset.iloc[:, 13].values
+    #Printing out the values of X --> Which contains the features
+    #                           y --> Which contains the target variable
+
+    # Encoding categorical data
+    # Now we encode the string values in the features to numerical values
+    # The only 2 values are Product Type and Region which need to converted into numerical data
+    from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+    labelencoder_X_1 = LabelEncoder()#creating label encoder object no. 1 to encode region name(index 1 in features)
+    X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])#encoding region from string to just 3 no.s 0,1,2 respectively
+    labelencoder_X_2 = LabelEncoder()#creating label encoder object no. 2 to encode product type name(index 2 in features)
+    X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])#encoding product type from string to just 2 no.s 0,1(onprem,cloud) respectively
+    #Now creating Dummy variables
+    onehotencoder = OneHotEncoder()
+    X = onehotencoder.fit_transform(X).toarray()
+    X = X[:, 1:]
+
+    # Splitting the dataset into the Training set and Test set
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+    # Feature Scaling
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
     return X_train, X_test, y_train, y_test
--- binary_search_networks/util.py
+++ binary_search_networks/util.py
@@ -1,16 +1,18 @@
-# Author: Isamu Isozaki, Shesh Dave 
+# Author: Isamu Isozaki, Shesh Dave
 # Date: 2020/11/10
 # Purpose: Utility function folders
-from binary_search_networks.binary_search_parser import binary_search_parser 
-
+from binary_search_parser import binary_search_parser
+import numpy as np
+import matplotlib.pyplot as plt
+import random
 # Input: list of arguments
 # Output: dictionary of arguments
 # Parses the arguments
-def parse_arguments(args):
-    arg_parser = binary_search_parser()
-    args, _ = arg_parser.parse_known_args(args)
-    args = vars(args)
-    return args
+# def parse_arguments(args):
+#     arg_parser = binary_search_parser()
+#     args, _ = arg_parser.parse_known_args(args)
+#     args = vars(args)
+#     return args
 
 
 """
@@ -19,7 +21,9 @@ def parse_arguments(args):
 @param None
 linear_search in an array
 """
-def linear_search(arr1,n):
+
+
+def linear_search(arr1, n):
     arr2 = []
     duplicate = False
     for i in range(len(arr1)):
@@ -33,8 +37,47 @@ def linear_search(arr1,n):
     else:
         print("n value not found")
 
+
+def generate_random_noise(amplitude):
+    return (2*random.random()-1)*amplitude
+
+# Input: Integer, n, number of points. Float, p, the proportional std of the point
+# Output: List of length n which is a distribution which is a cusp
+# Parses the arguments
+
+
+def get_cusp(n, a=1, x=0.5, gamma=0.0, p=2):
+    """
+    n: number of points
+    a: amplitude
+    x: ratio of the peak in the range
+    gamma: randomness
+    p: power
+    y = a(i-nx)^p + noise(gamma) where i from 0 to n
+    """
+    if not (0 < x < 1):
+        return
+    # For the cusp function, this function is y = ax^2  for the first half of n and a(x-n)^2 for the next half
+    output = []
+    # first half
+    for i in range(int(n*x)):
+        output.append(a*i**p + generate_random_noise(gamma))
+
+    # first half
+    for i in range(int(n*x), n):
+        output.append(a*((n-i)*x/(1-x))**p + generate_random_noise(gamma))
+
+    return output
+
+
 if __name__ == "__main__":
-    arr1 = [34,23,5,6,7,11,2,23,8,94,40,61,23,5]
-    print(arr1)
-    n = int(input("enter n value: "))
-    search(arr1, n)
+    # arr1 = [34,23,5,6,7,11,2,23,8,94,40,61,23,5]
+    # print(arr1)
+    # n = int(input("enter n value: "))
+    # search(arr1, n)
+    y1 = get_cusp(n=10, a=0.3, gamma=20, x=0.7, p=2)
+    plt.plot(y1)
+    # y2 = get_cusp(n=100000, a=0.7, gamma=0.01, x=0.7, p=.7)
+    # plt.plot(y1)
+
+    plt.show()
